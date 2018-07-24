8048243f294a8fc9365f09abc9f69201e08de8c1
Darya Bobko <bobko.dv@gmail.com>
2018-07-24 12:40:11 +0300

    date added

--- blog/src/components/BlogList/BlogList.js
+++ blog/src/components/BlogList/BlogList.js
@@ -16,17 +16,56 @@ class BlogList extends Component {
   //   };
   // }
 
+  tittleInput = (e) => {
+    this.changeState('newPostTittle', e.target.value);
+  }
+
+  authorInput = (e) => {
+    this.changeState('newPostAuthor', e.target.value);
+  }
+
+  contentInput = (e) => {
+    this.changeState('newPostContent', e.target.value);
+  }
+
+  changeState = (property, newValue) => {
+    this.state.setState({
+      property: newValue
+    });
+  }
+
+  AddNewPost = () => {
+    // var newPost = {
+    //   id: 
+    // }
+  }
+
   render() {
     return (
       <div className="BlogList-container">
         <h1 className="BlogList-generalTitle">Blog</h1>
         {list.map((item) =>
-          <Link to={"/post-details/" + item.id}>
-            <div className="BlogList-wrapper" key={item.id}>
+          <Link key={item.id} to={"/post-details/" + item.id}>
+            <div className="BlogList-wrapper" >
               <h2 className="BlogList-title">{item.title}</h2> 
               <p className="BlogList-author">{item.author}</p>
+              <span>{item.datetime.getDate() + "/" + item.datetime.getMonth() + "/" + item.datetime.getFullYear()}</span>
             </div>
           </Link>)}
+
+          {/* <form>
+              <label>
+                Tittle <input onChange={this.tittleInput}/>
+              </label>
+              <label>
+                Author <input onChange={this.authorInput}/>
+              </label>
+              <label>
+                Content <textarea onChange={this.contentInput}></textarea>
+              </label>
+
+              <button onClick={this.AddNewPost}>Add new</button>
+          </form> */}
       </div>
     );
   }
--- blog/src/components/PostDetails/PostDetails.js
+++ blog/src/components/PostDetails/PostDetails.js
@@ -22,7 +22,11 @@ class PostDetails extends Component {
   render() {
     return (
       <div className="PostDetails-container">
+      <div className="PostDetails-flex">
         <h1 className="PostDetails-title">{this.state.post.title}</h1>
+        <span>{this.state.post.datetime.getDate() + "/" + this.state.post.datetime.getMonth() + "/" + this.state.post.datetime.getFullYear()}</span>
+      </div>
+        
         <p className="PostDetails-text"> {this.state.post.text}</p>
         <p className="PostDetails-author">{this.state.post.author}</p>
         <button onClick={this.deletePost}>Delete</button>
